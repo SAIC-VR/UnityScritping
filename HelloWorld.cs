a3c8111e5cf8a4547af238818a92a3d069a58e5b
SAIC-VR <blopez3@saic.edu>
2018-10-02 14:19:18 -0500

    Fisrt commit
    uploaded a few example scripts

---
+++ HelloWorld.cs
@@ -0,0 +1,34 @@
+﻿// VR at SAIC 
+// UnityScritping
+// Basic scripts to get started with scripting in Unity 3D
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Class 
+
+public class HelloWorld : MonoBehaviour {
+
+// We will play and learn the difference between public and private variables
+
+	// public string text = "Hello World";
+	// public bool turnOn = true ;
+	// public float speed = 0.5f;
+	// public int boxes = 2;
+
+	// my first code exercise Yay!!
+
+	// Use this for initialization
+	void Start () {
+		print ("Hello world!");
+//		print (text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log(text);
+
+	}
+}
---
+++ debug.cs
@@ -0,0 +1,21 @@
+﻿// VR at SAIC 
+// UnityScritping
+// Basic scripts to get started with scripting in Unity 3D
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class debug : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hi I reached the start function");
+	//	Debug.Log ("Hi Imersive Envrionments!");
+	// This is a comment
+	}
+
+	void Update(){
+		// print ("Hi I reached the Update function");
+	}
+}
